a059392d3f02f3a73779c8d45ede439e2d170af2
Yakult <foreverswuad@gmail.com>
2025-12-09 12:40:13 +0700

    Change export syntax from default to module.exports

--- proxy.js
+++ proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).json({ error: "Missing url" });
 
@@ -16,4 +16,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).json({ error: err.message || "Proxy failed" });
   }
-}
+};
